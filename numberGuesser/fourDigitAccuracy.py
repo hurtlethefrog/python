9086c28cdc6208caf9a1c73197a9ab410001610d
Duncan H <duncan.haran@gmail.com>
2019-09-30 14:17:54 -0400

    game works

--- numberGuesser/fourDigitAccuracy.py
+++ numberGuesser/fourDigitAccuracy.py
@@ -1,18 +1,29 @@
 import random
-print("Guess a four digit number, for each correct number guessed at the correct index you will recieve a cow, and for each correct number guesed at the incorrect index you will recieve a bull.")
+print("Guess a four digit number, \nfor each correct number guessed at the correct index you will recieve a cow, \nand for each correct number guesed at the incorrect index you will recieve a bull.")
 
 def bovineAccuracy():
+    guessing = True
     num = str(random.randint(1000, 9999))
-    print(num)
-    cowBull = [0,0]
-    guess = input("enter your guess now: ")
-    if guess == num:
-        return print("You got it! Congrats!")
-    index = 0
-    while index < len(num):
-        if guess[index] == num[index]:
-            cowBull[0] += 1
-        index += 1   
-    print("cows:"+str(cowBull[0])+" bulls:"+str(cowBull[1]))
+    guesses = 0
+    while guessing == True:
+        cowBull = [0,0]
+        guess = input("enter your guess now: ")
+        guesses +=1
+        if guess == "num":
+            print(num)
+        if guess == num:
+            guessing = False
+            return print("You got it! Congrats! It only took you "+str(guesses)+" guesses!")
+        cowIndex = 0
+        while cowIndex < len(num):
+            if guess[cowIndex] == num[cowIndex]:
+                cowBull[0] += 1
+            cowIndex += 1
+        bullIndex = 0
+        while bullIndex < len(num):
+            if num.__contains__(guess[bullIndex]) and guess[bullIndex] != num[bullIndex]:
+                cowBull[1] += 1
+            bullIndex += 1
+        print("cows:"+str(cowBull[0])+" bulls:"+str(cowBull[1]))
 
 bovineAccuracy()
