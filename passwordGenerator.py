0202c957d4a875ee264c32457b36af33494b946e
Duncan H <duncan.haran@gmail.com>
2019-09-30 14:52:41 -0400

    one line deffining code goal

---
+++ passwordGenerator.py
@@ -0,0 +1,5 @@
+print("I'm going to generate a password for you, I'll need you to provide a key\nthe longer you make your key the stronger and longer you output password will be.")
+
+_key = input("enter your key here: ")
+
+def password_generator(key):
