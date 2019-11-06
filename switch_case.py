443da5a1f65ddb2039b5007aa866e600e79816a8
Duncan H <duncan.haran@gmail.com>
2019-11-06 12:43:59 -0500

    an improvised switch statement

---
+++ switch_case.py
@@ -0,0 +1,19 @@
+switcher = {
+   1: "January",
+   2: "February",
+   3: "March",
+   4: "April",
+   5: "May",
+   6: "June",
+   7: "July",
+   8: "August",
+   9: "September",
+   10: "October",
+   11: "November",
+   12: "December"
+   }
+
+def what_month(num):
+    return switcher[num]
+
+print(what_month(7))
