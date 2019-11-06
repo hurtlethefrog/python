6b2bbc7c28709e7c00d38cce3b05b59fc86c8a8c
Duncan H <duncan.haran@gmail.com>
2019-11-06 14:30:47 -0500

    unpacking

---
+++ tupples.py
@@ -0,0 +1,12 @@
+numbers = (1, 2, 3, 4)
+
+numbers = (1, 2, 3, 4, 5)
+
+print(numbers)
+
+# essentially just locks out list methods, not equivalent to const
+
+coords = [1, 2, 3]
+
+x, y, z = coords
+print(x)
