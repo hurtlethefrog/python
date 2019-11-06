ff6c7214035d488da78ae15744409b3d14ca0e76
Duncan H <duncan.haran@gmail.com>
2019-11-06 12:56:33 -0500

    if statements, so cool

--- weight_converter.py
+++ weight_converter.py
@@ -1,4 +1,10 @@
-weight_lbs = input("what is your weith in lbs?:")
-weight_kg = int(weight_lbs) / 2.20462
+import math
 
-print("your weight in kg:", weight_kg)
+weight = input("what is your weight?:")
+unit = input('was that lbs or kgs?(L/K):')
+if unit.upper() == 'L':
+    weight_kg = int(weight) / 2.20462
+    print("your weight in kg:", math.trunc(weight_kg))
+elif unit.upper() == 'K':
+    weight_lbs = int(weight) * 2.20462
+    print(f'your weight in lbs: {math.trunc(weight_lbs)}')
