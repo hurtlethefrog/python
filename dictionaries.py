8f826bd5330099ff136445e1fe2a6930e346e1e3
Duncan H <duncan.haran@gmail.com>
2019-11-07 13:07:46 -0500

    dictionaries and exceptions

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+#  yay objects
+
+dic = {
+    'name': 'duncan',
+    'email': 'email@email.com', 
+    'phone': '123 456 7890'
+}
+
+print(dic.get('name'))
---
+++ exceptions.py
@@ -0,0 +1,5 @@
+try:
+    number = input('type a number:')
+    print(number / 2)
+except TypeError:
+    print('I said number')
