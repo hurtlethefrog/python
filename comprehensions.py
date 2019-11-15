f3b2e21ba1d4d0773e796a6e4aa3b64d5ab7bb57
Duncan H <duncan.haran@gmail.com>
2019-11-15 17:33:06 -0500

    comprehensions with dicts

--- comprehensions.py
+++ comprehensions.py
@@ -1,5 +1,10 @@
 numbers = [1, 2, 3, 4, 5]
+days = ['mon', 'tues', 'wed']
 
 squared_list = [n*n for n in numbers]
 
 print(squared_list)
+
+date_dict = {num: day for num, day in zip(numbers, days)}
+
+print(date_dict)
