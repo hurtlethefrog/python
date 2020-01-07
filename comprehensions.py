129279673b6d74e71aed3616f33ec4cd3fa31f0b
Duncan H <duncan.haran@gmail.com>
2020-01-07 18:35:02 -0500

    messed with comprehensions

--- comprehensions.py
+++ comprehensions.py
@@ -1,10 +1,12 @@
 numbers = [1, 2, 3, 4, 5]
 days = ['mon', 'tues', 'wed']
 
-squared_list = [n*n for n in numbers]
+[print(n*n) for n in numbers]
 
-print(squared_list)
+# print(squared_list)
 
-date_dict = {num: day for num, day in zip(numbers, days)}
+# date_dict = {num: day for num, day in zip(numbers, days)}
 
-print(date_dict)
+# print(date_dict)
+
+# print(list(n*n for n in numbers))
